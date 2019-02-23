e0c38ad250e21c441ef68a202157a5322ce1f9f0
mikezalik <mike.zalik@gmail.com>
2019-02-22 17:10:20 -0800

    nodemon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send();
+    response.send("I love Express!");
 });
 
 app.listen(3000);
