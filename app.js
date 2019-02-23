c0ad3ddf662308826ffe9c8e4452285116b7af39
mikezalik <mike.zalik@gmail.com>
2019-02-22 23:18:21 -0800

    cookie

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    if (name) {
+    res.render('index', {name});
+    } else {
+        res.redirect('/hello');
+    }
 });
 
 app.get('/cards', (req, res) => {
@@ -13,11 +21,12 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-    res.render('hello');
+    res.render('hello';
 })
 
 app.post('/hello', (req, res) => {
-    res.render('hello');
+    res.cookie('username', req.body.username);
+    res.redirect('/');
 })
 
 app.listen(3000, () => {
