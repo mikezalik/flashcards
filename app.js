24075cc27b2b96994433fb39e381b37e5fcd2162
mikezalik <mike.zalik@gmail.com>
2019-03-11 22:20:58 -0700

    card routes

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.set('view engine', 'pug');
 
-const routes = require('./routes')
+const mainRoutes = require('./routes')
+const cardRoutes = require('./routes/cards')
 
-app.use(routes);
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 app.use((req,res,next) => {
     console.log(req.message);
