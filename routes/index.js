5d0d23e22fba1a00d5741dc94833dc9eb67cb31a
mikezalik <mike.zalik@gmail.com>
2019-03-11 22:14:03 -0700

    route modularization

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+  res.render('index', {name});
+  } else {
+      res.redirect('/hello');
+  }
+});
+
+router.get('/cards', (req, res) => {
+  res.render('card', {prompt: "Who is buried in Grant's tomb?", hint: "Think about it..."});
+});
+
+router.get('/hello', (req, res) => {
+  res.render('hello');
+})
+
+router.post('/hello', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/');
+})
+
+router.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
+})
