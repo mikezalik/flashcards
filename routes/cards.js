4cdc8e7879d40bd1eee680c9b994819e8fe9a6ef
mikezalik <mike.zalik@gmail.com>
2019-04-09 21:10:07 -0700

    cards.js

--- routes/cards.js
+++ routes/cards.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get( '/', (req, res) => {
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor(Math.random() * numberOfCards);
+  res.redirect( `/cards/${flashcardId}?side=question`)
+});
 
 router.get('/:id', (req, res) => {
   const { side } = req.query;
