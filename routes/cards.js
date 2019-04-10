f054655233ec502baaa21dfe6ccda7f0d02bf7b6
mikezalik <mike.zalik@gmail.com>
2019-04-09 21:14:24 -0700

    cards.js

--- routes/cards.js
+++ routes/cards.js
@@ -6,12 +6,17 @@ const { cards } = data;
 router.get( '/', (req, res) => {
   const numberOfCards = cards.length;
   const flashcardId = Math.floor(Math.random() * numberOfCards);
-  res.redirect( `/cards/${flashcardId}?side=question`)
+  res.redirect( `/cards/${flashcardId}`)
 });
 
 router.get('/:id', (req, res) => {
   const { side } = req.query;
   const { id } = req.params;
+
+if (!side) {
+  res.redirect(`/cards/${id}?side=question`);
+}
+
   const text = cards[id][side];
   const { hint } = cards[id];
   const templateData = { text };
