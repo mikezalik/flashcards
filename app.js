a4409163778306d273a1f5ac081171693f824a41
mikezalik <mike.zalik@gmail.com>
2019-05-02 22:27:00 -0700

    JS edit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use('/staic', express.static('public'));
+app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 
