ce3b2706578e37a376cbb4ecec950d074c4e60d9
mikezalik <mike.zalik@gmail.com>
2019-06-22 15:56:00 -0700

    cards fix, index refactor

--- routes/cards.js
+++ routes/cards.js
@@ -3,37 +3,35 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
-router.get( '/', (req, res) => {
+router.get( '/', ( req, res ) => {
   const numberOfCards = cards.length;
-  const flashcardId = Math.floor(Math.random() * numberOfCards);
-  res.redirect( `/cards/${flashcardId}`)
+  const flashcardId = Math.floor( Math.random() * numberOfCards );
+  res.redirect( `/cards/${flashcardId}` )
 });
 
 router.get('/:id', (req, res) => {
-  const { side } = req.query;
-  const { id } = req.params;
+    const { side } = req.query;
+    const { id } = req.params;
 
-if (!side) {
-  return res.redirect(`/cards/${id}?side=question`);
-}
-  const name = req.cookies.username;
-  const text = cards[id][side];
-  const { hint } = cards[id];
+    if ( !side ) {
+        return res.redirect(`/cards/${id}?side=question`);
+    }
+    const name = req.cookies.username;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+    
+    const templateData = { id, text, name, side };
 
-  const templateData = { text };
+    if ( side === 'question' ) {
+      templateData.hint = hint;
+      templateData.sideToShow = 'answer';
+      templateData.sideToShowDisplay = 'Answer';
+    } else if ( side === 'answer' ) {
+      templateData.sideToShow = 'question';
+      templateData.sideToShowDisplay = 'Question';
+    }
 
-  const templateData = {id, text};
-
-  if (side === 'question') {
-    templateData.hint = hint;
-    templateData.sideToShow = 'answer';
-    templateData.sideToShowDisplay = 'Answer';
-  } else if (side === 'answer') {
-    templateData.sideToShow = 'question';
-    templateData.sideToShowDisplay = 'Question';
-  }
-
-  res.render('card', templateData);
+    res.render('card', templateData);
 });
 
 module.exports = router;
