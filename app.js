8f390db16c4e457614d4ba58713bbfe888e68f37
mikezalik <mike.zalik@gmail.com>
2019-06-25 20:55:30 -0700

    refactor

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/static', express.static('public'));
